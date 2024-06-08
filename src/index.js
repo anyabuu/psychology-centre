04e543ba61b0911f9e91f9b316fb120ea656f163
Anya <81263768+anyabuu@users.noreply.github.com>
2024-06-08 22:37:54 +0300

    git inited

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const accordions = document.querySelectorAll('.questions__item');
+
+accordions.forEach(function(item){
+    item.addEventListener('click', function() {
+        if (this.classList.contains('show')) {
+            this.classList.remove('show');
+        } else {
+            accordions.forEach(function (subItem) {
+                subItem.classList.remove('show');
+            })
+
+            this.classList.add('show');
+        }
+    })
+});
