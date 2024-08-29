00904bd9162b15fb0b5936565b7300878faeaef5
Anya <81263768+anyabuu@users.noreply.github.com>
2024-08-29 17:57:31 +0300

    burger menu

--- src/index.js
+++ src/index.js
@@ -17,6 +17,22 @@ document.addEventListener('DOMContentLoaded', (event) => {
       }
     })
   });
+});
 
+const menuBtn = document.querySelector('.header__menu-bar');
+const menu = document.querySelector('.header__burger');
 
+document.addEventListener('DOMContentLoaded', (event) => {
+  if (menuBtn) {
+    menuBtn.addEventListener('click', function () {
+      menu.classList.toggle('active');
+      menuBtn.classList.toggle('active');
+    });
+  }
 })
+
+
+
+
+
+
